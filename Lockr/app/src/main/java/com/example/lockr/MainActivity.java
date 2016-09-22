19ea4f04414dae8d566f400ca7ad905809f2c256
Nathan M <nathan.muckley@gmail.com>
2016-09-22 15:54:09 +1200

    Added lockscreen service

--- Lockr/app/src/main/java/com/example/lockr/MainActivity.java
+++ Lockr/app/src/main/java/com/example/lockr/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.lockr;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
+import android.widget.CompoundButton;
 import android.content.Context;
+import com.example.lockscreen.Lockscreen;
+import com.example.lockscreen.SharedPreferencesUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +22,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mContext = this;
         setContentView(R.layout.activity_main);
+        SharedPreferencesUtil.init(mContext);
+        checkDrawOverlayPermission();
 
+        mSwitchd = (SwitchCompat) this.findViewById(R.id.switch_locksetting);
+        mSwitchd.setTextOn("yes");
+        mSwitchd.setTextOff("no");
+        boolean lockState = SharedPreferencesUtil.get(Lockscreen.ISLOCK);
+        if (lockState) {
+            mSwitchd.setChecked(true);
+
+        } else {
+            mSwitchd.setChecked(false);
+
+        }
+
+        mSwitchd.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    SharedPreferencesUtil.setBoolean(Lockscreen.ISLOCK, true);
+                    Lockscreen.getInstance(mContext).startLockscreenService();
+                } else {
+                    SharedPreferencesUtil.setBoolean(Lockscreen.ISLOCK, false);
+                    Lockscreen.getInstance(mContext).stopLockscreenService();
+                }
+
+            }
+        });
+    }
+
+    /** code to post/handler request for permission */
+    public final static int REQUEST_CODE = 5463;
+
+    public void checkDrawOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /** check if we already  have permission to draw over other apps */
+            if (!Settings.canDrawOverlays(this)) {
+                /** if not construct intent to request permission */
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                /** request permission via start activity for result */
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /** check if received result code
+             is equal our requested code for draw permission  */
+            if (requestCode == REQUEST_CODE) {
+                /** if so check once again if we have permission */
+                if (Settings.canDrawOverlays(this)) {
+                    // continue here - permission was granted
+                }
+            }
+        }
     }
 
     @Override
